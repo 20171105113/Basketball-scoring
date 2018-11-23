7f5dae91422365e6344637025d4e3e45add5977c
20171105113 <wy1239072462@163.com>
2018-11-23 10:04:37 +0800

    1.1
    增加了分钟计时，配合秒钟计时

--- Basketball scoring device/ViewController.swift
+++ Basketball scoring device/ViewController.swift
@@ -10,11 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var leftTime:Int = 60
+    var leftTime:Int = 0
+    var leftTime2:Int = 0
     var timer :Timer!
+    var timer2 :Timer!
     var homepoints:Int = 0
     var awaypoints:Int = 0
+    var temp:String = "0"
+    var temp2:String = "0"
     @IBOutlet weak var Timedisplay: UITextField!
+    @IBOutlet weak var Timedisplay2: UITextField!
     @IBOutlet weak var Homepoints: UITextField!
     @IBOutlet weak var Awaypoints: UITextField!
     override func viewDidLoad() {
@@ -24,12 +29,24 @@ class ViewController: UIViewController {
 
 
 
+    
+    
     @IBAction func Startone(_ sender: Any) {
+        temp = Timedisplay.text!
+        leftTime = Int(temp)!
         Timedisplay.isEnabled =  false
         timer = Timer.scheduledTimer(timeInterval: TimeInterval(1), target: self, selector: #selector(tickDown), userInfo: nil, repeats: true)
+        
+        temp2 = Timedisplay2.text!
+        leftTime2 = Int(temp2)!
+        Timedisplay2.isEnabled = false
+        timer2 = Timer.scheduledTimer(timeInterval: TimeInterval(60), target: self, selector: #selector(tickDown2), userInfo: nil, repeats: true)
+        
+        
     }
     @IBAction func Finishone(_ sender: Any) {
         timer.invalidate()
+        timer2.invalidate()
     }
     
     @IBAction func Homebonuspoints(_ sender: Any) {
@@ -40,15 +57,47 @@ class ViewController: UIViewController {
     
     @IBAction func Awaybonuspoints(_ sender: Any) {
         awaypoints = awaypoints + 1
+        Awaypoints.text = "\(awaypoints)"
+    }
+    @IBAction func Homebonus2(_ sender: Any) {
+        homepoints = homepoints + 2
         Homepoints.text = "\(homepoints)"
+    }
+    @IBAction func Homebonus3(_ sender: Any) {
+        homepoints = homepoints + 2
+        Homepoints.text = "\(homepoints)"
+    }
+    @IBAction func Awaybonus2(_ sender: Any) {
+        awaypoints = awaypoints + 2
+        Awaypoints.text = "\(awaypoints)"
+    }
+    @IBAction func Awaybonus3(_ sender: Any) {
+        awaypoints = awaypoints + 3
+        Awaypoints.text = "\(awaypoints)"
     }
     @objc func tickDown() {
         leftTime -= 1
         print(leftTime)
         Timedisplay.text = "\(leftTime)"
         if leftTime <= 0{
-             timer.invalidate()
+             leftTime = 60
         }
-}
+        if leftTime2 == -1{
+            timer.invalidate()
+        }
 
+        
+}
+    @objc func tickDown2() {
+            leftTime2 -= 1
+
+        
+
+            print(leftTime2)
+            Timedisplay2.text = "\(leftTime2)"
+            if leftTime2 <= 0{
+                timer2.invalidate()
+                leftTime2 = -1
+            }
+}
 }

bb16cbaa59da56067697442ab77df13fbf030227
20171105113 <wy1239072462@163.com>
2018-12-20 11:35:04 +0800

    1.3
    界面传值还没有正常实现，目前正在测试工程中测试界面传值。

--- Basketball scoring device/ViewController.swift
+++ Basketball scoring device/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var leftTime:Int = 0
+    var leftTime:Int = 60
     var leftTime2:Int = 0
     var timer :Timer!
     var timer2 :Timer!
@@ -18,6 +18,7 @@ class ViewController: UIViewController {
     var awaypoints:Int = 0
     var temp:String = "0"
     var temp2:String = "0"
+    var abc:String = "ABC"
     @IBOutlet weak var Timedisplay: UITextField!
     @IBOutlet weak var Timedisplay2: UITextField!
     @IBOutlet weak var Homepoints: UITextField!
@@ -25,12 +26,13 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
 
 
     
-    
+    //开始计时
     @IBAction func Startone(_ sender: Any) {
         temp = Timedisplay.text!
         leftTime = Int(temp)!
@@ -44,6 +46,7 @@ class ViewController: UIViewController {
         
         
     }
+    //停止计时
     @IBAction func Finishone(_ sender: Any) {
         timer.invalidate()
         timer2.invalidate()
@@ -52,9 +55,8 @@ class ViewController: UIViewController {
     @IBAction func Homebonuspoints(_ sender: Any) {
         homepoints = homepoints + 1
         Homepoints.text = "\(homepoints)"
-        
     }
-    
+   //加分
     @IBAction func Awaybonuspoints(_ sender: Any) {
         awaypoints = awaypoints + 1
         Awaypoints.text = "\(awaypoints)"
@@ -77,13 +79,14 @@ class ViewController: UIViewController {
     }
     @objc func tickDown() {
         leftTime -= 1
-        print(leftTime)
         Timedisplay.text = "\(leftTime)"
         if leftTime <= 0{
              leftTime = 60
         }
         if leftTime2 == -1{
             timer.invalidate()
+            Timedisplay.text = ""
+            Timedisplay2.text = ""
         }
 
         
@@ -95,9 +98,22 @@ class ViewController: UIViewController {
 
             print(leftTime2)
             Timedisplay2.text = "\(leftTime2)"
-            if leftTime2 <= 0{
+            if leftTime2 <= -1{
                 timer2.invalidate()
-                leftTime2 = -1
+                
             }
 }
+    //第一个界面
+    
+    @IBOutlet weak var Firstteam: UITextField!
+    @IBOutlet var Firstteaml: UIView!
+
+    @IBOutlet weak var team12: UILabel!
+    @IBAction func right(_ sender: Any) {
+        
+    }
+    @IBAction func test1(_ sender: UIButton) {
+         team12.text = abc
+    }
+   
 }
